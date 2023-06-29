d60bf960569e7d5eef333845822e764149940b4f
Abhishek <chettriabhisek0562000@gmail.com>
2023-06-29 23:25:31 +0530

    [Abhishek] Impliment Employee Wage using switch

--- EmployeeWageComputationDay9/Program.cs
+++ EmployeeWageComputationDay9/Program.cs
@@ -8,21 +8,35 @@ namespace EmployeeWageComputationDay9
 {
     internal class Program
     {
+        public static int IS_PART_TIME { get; private set; }
+
         static void Main(string[] args)
         {
-            const int IS_PRESENT = 1;
-            Console.WriteLine("Welcome to EmployeeWageCompute");
+            //Constants
+            int IS_PART_TME = 1;
+            int FULL_TIME = 2;
+            int EMP_RATE_PER_HR = 20;
+            //Variables
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
-            int randomInput = random.Next(0, 2); //0 or 1
-            if (IS_PRESENT == randomInput)
+            //Computation
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
             {
-                Console.WriteLine("Employee is present");
+                empHrs = 4;
             }
-            else
+            else if (empCheck == FULL_TIME)
             {
-                Console.WriteLine("Employee is absent");
+                empHrs = 8;
             }
 
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = EMP_RATE_PER_HR * empHrs;
+            Console.WriteLine("Employee Wage per day:{0}", empWage);
             Console.ReadLine();
 
         }
